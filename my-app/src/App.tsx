d66ad1ce5f1fd9a4b3f6107e0abbc11c1d3742a6
sck <whhan03@gmail.com>
2026-07-04 16:22:21 +0900

    typescript, 상태관리, Todo

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,10 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Counter from './components/Counter'
 import DrinkList from './components/DrinkList'
 import InputValueState from './components/InputValueState'
+import Todos from './placeholder/Todos'
+import TodoDetail from './placeholder/TodoDetail'
 // import Greet from './components/Greet'
 // import Player from './components/Player'
 // import Player2 from './components/Player2'
@@ -22,13 +25,20 @@ function App() {
   return (
     <>
       <section className="app">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Todos />} />
+            <Route path='/todos/:id' element={<TodoDetail />} />
+          </Routes>
+        </BrowserRouter>
         {/* <h2>TypeScript 기반 React 프로젝트</h2> */}
         {/* <Greet name="타입스크립트" /> */}
         {/* <Player player={playerObj} clickHandler={clickHandler} /> */}
         {/* <Player2 player={playerObj} clickHandler={clickHandler} /> */}
         {/* <Counter /> */}
         {/* <InputValueState /> */}
-        <DrinkList />        
+        {/* <DrinkList />         */}
+
 
       </section>
 
--- my-app/src/components/DrinkList.tsx
+++ my-app/src/components/DrinkList.tsx
@@ -3,8 +3,16 @@ import { useState } from "react";
 const DrinkList = () => {
     const [drinks, setDrinks] = 
         useState<string[]>(["사이다", "녹차", "주스"]);
+    // 음료 추가 함수
+    const addDrink = () => {
+        const newDrink = prompt("추가할 음료를 입력하세요: ");
+        if(newDrink) {
+            // 기존 배열에 새 음료를 추가
+            setDrinks([...drinks, newDrink]);
+        }
+    }
     return (
-        <div>
+        <div className="drinklist">
             <h2>음료 리스트</h2>
             <ul>
                 {drinks.map( (drink, index) => (
@@ -12,6 +20,7 @@ const DrinkList = () => {
 
                 ))}
             </ul>
+            <button onClick={addDrink}>음료 추가</button>
         </div>
     )
 }
